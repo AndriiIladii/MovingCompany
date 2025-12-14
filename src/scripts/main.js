caf9d3b4b6f10e3e3d9a90256b02bee4539c8207
AndriiIladii <iladiy21@gmail.com>
2025-12-15 01:51:44 +0200

    added responsive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,11 @@
 import "../styles/main.scss";
 
-// scripts/main.js
-import "../styles/main.scss";
-
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: true,
 
-  slidesPerView: 3,
+  loop: true,
   centeredSlides: true,
-
-  spaceBetween: 90,
-  loopedSlides: 6,
   roundLengths: true,
   observer: true,
   observeParents: true,
@@ -24,5 +18,27 @@ const swiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1.15,
+      spaceBetween: 15,
+    },
+
+    640: {
+      slidesPerView: 1.5,
+      spaceBetween: 70,
+    },
+
+    1024: {
+      slidesPerView: 2.5,
+      spaceBetween: 70,
+    },
+
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 90,
+    },
   },
 });
